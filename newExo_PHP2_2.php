c476bf9719e788c10c738979cb765560b35e81fa
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-18 15:35:58 +0200

    Correction Exo 3

--- newExo_PHP2_2.php
+++ newExo_PHP2_2.php
@@ -12,23 +12,38 @@ une fonction personnalisée.<br>
 <p>Affichage demandé<br>
 <br>Un tableau associatif intégrant les Pays (1ère colonne) et leurs capitales respectives (2ème colonne)</p>
 
+ <!-- Créer le Tableau (appeler la fonction "table" & avec "Border" Définir le contour -->
 <table border=1>
 
 <!--Entête du Tableau -->
     <thead>
-        <tr>
-            <th>Pays</th>
-            <th>Capitales</th>
+        <tr><!-- Créer les cases de l'entête du tableau -->
+            <th>Pays</th> <!-- Créer la 1ère cellule (1ère colonne) et la compléter par le mot souhaité -->
+            <th>Capitales</th> <!-- Créer la 2ème cellule (2ème colonne) et la compléter par le mot souhaité -->
         </tr>
     </thead>
 
 <!--Corps du Tableau -->
 <body>
     <tr>
-        <td>France</td>
+        <td>France</td> <!--Créer plusieurs td groupés avec td*4 par exemple (apparition de 4 td en auto) -->
         <td>Paris</td>
         <td></td>
         <td></td>
+    </tr>
+    
+    <tr>
+        <td>Allemagne</td> 
+        <td>Berlin</td>
+    </tr>
+    <tr>
+        <td>USA</td> 
+        <td>Washington</td>
+    </tr>
+    
+    <tr>
+        <td>Italie</td>
+        <td>Rome</td>
     </tr>
 </body>
 </table>
---
+++ newExo_PHP2_3.php
@@ -0,0 +1,14 @@
+<h1>Exerice n°3 PHP 2</h1>
+
+<h2>Afficher un lien hypertexte<br></h2>
+
+<p>Consigne demandée : <br>Le lien doit permettre d'accéder au site d'Elan Formation. <br>
+<br>Particularité demandée :<br>
+<br>Le lien devra s’ouvrir dans un nouvel onglet (_blank).<br></p>
+
+<?php
+
+#afficher un lien vers le site Pc Activity
+echo "<a href=https://www.elan-formation.fr .target='_blank'>Elan Formation</a>";
+
+?>
