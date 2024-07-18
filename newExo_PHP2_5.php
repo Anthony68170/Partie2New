0db98d688df9443d24234c4fcfbd9af7b9e8fc58
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-18 17:05:10 +0200

    Modification Exo5

--- newExo_PHP2_5.php
+++ newExo_PHP2_5.php
@@ -12,7 +12,18 @@
 <!-- Il faut donc d'abord créer le tableau (partie HTML affichant une structure)
  Puis PHP pour obtenir des données (avec des champs à compléter) -->
 
- 
+ <head><title>Formulaire de saisie utilisateur </title></head>
+    <body>
+        <h1>Inscrivez-vous !</h1>
+        <h2>Entrez les données demandées :</h2>
+        <form name="inscription" method="post" action="form.php">
+            Entrez votre pseudo : <input type="text" name="pseudo"/> <br/>
+            Garçon ou fille ? 	<input type="radio" name="sexe" value="G"/>Garçon<input type="radio" name="sexe" value="F"/>Fille<br/>
+            Entrez votre age : <input type="text" name="age"/><br/>
+            <input type="submit" name="valider" value="OK"/>
+        </form>
+    </body>
+    
 <?php
 
 
