d3f4e8766614c7cde37fb4f26504df48a3c0af9d
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-07-21 00:58:37 +0200

    Modif crea colonne

---
+++ newExo_PHP2_4Bis.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<h1>Tableau indexé html & associatif PHP</h1>
+<body>
+    <table border="1">
+    <tr>
+        <th>Pays</th>
+    </tr>
+</body>
+</html>
